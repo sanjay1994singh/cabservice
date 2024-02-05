369274af6b22ba295b5be011651f426dfa4c3200
nidhivan ras <nidhivanras@gmail.com>
2024-02-05 22:16:35 +0530

    p

---
+++ car_and_rent/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarAndRentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'car_and_rent'
---
+++ tourist_places/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TouristPlacesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tourist_places'
