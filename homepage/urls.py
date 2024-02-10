b5569ac865ff5cb6922c694d1338b1f87e046a20
nidhivan ras <nidhivanras@gmail.com>
2024-02-10 14:15:18 +0530

    p

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('book_cab/', views.book_cab, name='book_cab'),
+    path('hotels/', views.hotels, name='hotels'),
+    path('tourist_place/', views.tourist_place, name='tourist_place'),
+    path('videos/', views.videos, name='videos'),
+    path('contact_us/', views.contact_us, name='contact_us'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -17,3 +17,35 @@ def homepage(request):
         'video': video,
     }
     return render(request, 'index-4.html', context)
+
+
+def hotels(request):
+    hotel = Hotels.objects.all()
+    context = {
+        'hotel': hotel,
+    }
+    return render(request, 'hotel.html', context)
+
+
+def book_cab(request):
+    return render(request, 'book_cab.html')
+
+
+def tourist_place(request):
+    place = TouristPlace.objects.all()
+    context = {
+        'place': place,
+    }
+    return render(request, 'tourist_place.html', context)
+
+
+def videos(request):
+    video = Video.objects.all()
+    context = {
+        'video': video,
+    }
+    return render(request, 'videos.html', context)
+
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
